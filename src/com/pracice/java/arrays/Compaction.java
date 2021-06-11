789408fcf56ec3f683487019bcec5830ad228abb
Murugeswari Selvaraj <Murugeswari.Selvaraj@deliveryblueprints.com>
2021-06-11 21:56:37 +0530

    Compaction, MaximumProduct

---
+++ src/com/pracice/java/arrays/Compaction.java
@@ -0,0 +1,42 @@
+package com.pracice.java.arrays;
+public class Compaction {
+	//Move all the running process and empty blocks to the end so that continuous blocks can be alloted next
+	//Means move all non zero elements to front and zero elements to the back of the array
+	
+	public int[] doCompaction(int[] blocks) {
+		if(blocks.length == 0) {
+			return blocks;
+		}
+		
+		//when you find non-zero element swap it with zero element (takes O(n) time and O{1) space)
+		int lastZeroPosition = -1;
+		for (int i=0; i< blocks.length;i++) {
+			if(blocks[i] != 0) {
+				swap(blocks, ++lastZeroPosition, i);
+			}
+		}
+		printBlocks(blocks);
+		return blocks;
+	}
+	
+	public void swap(int[] array, int source, int target) {
+		int temp = array[source];
+		array[source] = array[target];
+		array[target] = temp;
+	}
+	
+	public void printBlocks(int[] blocks) {
+		System.out.println();
+		for (int i=0; i< blocks.length;i++) {
+			System.out.print(blocks[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Compaction compaction = new Compaction();
+		compaction.doCompaction(new int[] {1, 2, 0, 0, 3, 0, 5});
+		compaction.doCompaction(new int[] {1, 2, 3, 4, 5, 6, 7});
+		compaction.doCompaction(new int[] {0, 0, 0, 0, 0, 0, 0});
+		compaction.doCompaction(new int[] {0, 1, 2, 3, 4, 5, 6});
+	}
+}
---
+++ src/com/pracice/java/arrays/MaximumProduct3Numbers.java
@@ -0,0 +1,40 @@
+package com.pracice.java.arrays;
+
+public class MaximumProduct3Numbers {
+	
+	public int findMaxProduct(int[] array) {
+		int max1 = Integer.MIN_VALUE;
+		int max2 = Integer.MIN_VALUE;
+		int max3 = Integer.MIN_VALUE;
+		int min1 = Integer.MAX_VALUE;
+		int min2 = Integer.MAX_VALUE;
+		
+		for (int i=0; i<array.length; i++) {
+			if(array[i] > max1) {
+				max3 = max2;
+				max2 = max1;
+				max1 = array[i];
+			} else if(array[i] > max2) {
+				max3 = max2;
+				max2 = array[i];
+			} else if(array[i] > max3) {
+				max3 = array[i];
+			} 
+			if(array[i] < min1) {
+				min2 = min1;
+				min1 = array[i];
+			} else if(array[i] < min2) {
+				min2 = array[i];
+			}
+		}
+		System.out.println(String.format("max1 %d, max2 %d, max3 %d, min1 %d, min2 %d",  max1, max2, max3, min1, min2));
+		return Math.max(min1*min2*max1, max1*max2*max3);
+	}
+	
+	public static void main(String[] args) {
+		MaximumProduct3Numbers maximumProduct3Numbers = new MaximumProduct3Numbers();
+		System.out.println(maximumProduct3Numbers.findMaxProduct(new int[]{1,2,5,9,3,2,10}));
+		System.out.println(maximumProduct3Numbers.findMaxProduct(new int[]{-10,2,5,-20,3,2,10}));
+		System.out.println(maximumProduct3Numbers.findMaxProduct(new int[]{-10,2,-5,-20,3,2,10}));
+	}
+}
